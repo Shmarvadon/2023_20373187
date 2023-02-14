522e5b86065b1f8ca0c46c7b6356ebb3023dc4a3
ShmarvDogg <harveyjohnson121@yahoo.com>
2023-02-14 14:22:37 +0000

    Completing Work Sheet 4

---
+++ WS4_Exercises/Exercise 3/adder.h
@@ -0,0 +1,18 @@
+// A better way of header guarding.
+#pragma once
+
+#if defined(WIN32)|defined(_WIN32)
+	#ifdef maths_STATIC
+		#define MATHSLIB_API
+	#else
+		#ifdef maths_EXPORTS
+			#define MATHSLIB_API __declspec( dllexport )
+		#else 
+			#define MATHSLIB_API __declspec( dllimport )
+		#endif
+	#endif
+#else 
+	#define MATHSLIB_API
+#endif
+
+MATHSLIB_API int add(int a, int b);
